c691a8d3d121c761ee44e2c9be053611d2ab95cd
UmidIsanov <isanovumid8@gmail.com>
2024-05-13 13:54:16 +0500

    feat: add some new things

--- src/screens/ActorScreen.js
+++ src/screens/ActorScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import { useGetPersonByIdQuery } from "../store/api/peopleApi";
 import { useGetPersonOwnDataByIdQuery } from "../store/api/moviesApi";
-
+import { getPoster } from "../services/MovieService";
+import IMAGES from "../constants/Images";
 const ActorScreen = ({ navigation, route }) => {
   const { data, isLoading, error } = useGetPersonOwnDataByIdQuery(
     route.params.actorId
   );
+  console.log(data);
 
   if (isLoading) {
     return (
@@ -27,7 +29,15 @@ const ActorScreen = ({ navigation, route }) => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>ActorScreen</Text>
+      <Image
+        source={
+          data.profile_path
+            ? { uri: getPoster(data.profile_path) }
+            : IMAGES.NO_IMAGE
+        }
+        style={{ width: 80, height: 120 }}
+        resizeMode={data.profile_path ? "cover" : "contain"}
+      />
       {data && <Text>{data.name}</Text>}
       <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
